891668dc62f0897ab5b9ed15dae0db3f26965ebe
worldwidesorrow <jsnmerrill@yahoo.com>
2017-03-30 00:04:55 -0500

    Delete Parts.hpp

--- CfgLoot/Groups/Parts.hpp
+++
@@ -1,8 +0,0 @@
-Parts[] =
-{
-	{Loot_MAGAZINE,		10,		PartGeneric},
-	{Loot_MAGAZINE,		10,		PartGlass},
-	{Loot_MAGAZINE,		10,		PartFueltank},
-	{Loot_MAGAZINE,		10,		PartWheel},
-	{Loot_MAGAZINE,		10,		PartEngine}
-};
